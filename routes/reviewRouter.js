741c20dbe2e1fd5bc9b6c608742c8fe76385a69a
shalukap <jude.shaluka.perera@gmail.com>
2025-01-07 09:24:13 +0530

    update review router

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { addReview,getReview,deleteReview } from '../controllers/reviewController.js';
+import { addReview,getReview,deleteReview,approveReview } from '../controllers/reviewController.js';
 
 const reviewRouter=express.Router();
 
 reviewRouter.post("/",addReview)
 reviewRouter.get("/",getReview)
 reviewRouter.delete("/:email",deleteReview)
+reviewRouter.put("/approve/:email",approveReview)
 
 export default reviewRouter;
